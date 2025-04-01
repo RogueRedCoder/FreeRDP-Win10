ed92f13acd3369dfc532bcc9a9cc9caf1f3b0450
AloneBoredDeveloper <irodentv@gmail.com>
2025-04-01 22:38:13 +0330

    Update wall.bat

--- wall.bat
+++ wall.bat
@@ -1,8 +1,8 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
+powershell -Command "Invoke-WebRequest https://aloneboreddeveloper.github.io/site/wp-content/uploads/2025/02/0547b64597cb21f35386e205556af466-1.jpg -OutFile 0547b64597cb21f35386e205556af466-1.jpg"
 powershell -Command "Invoke-WebRequest https://aloneboreddeveloper.github.io/site/wp-content/uploads/2025/02/0547b64597cb21f35386e205556af466-1.jpg -OutFile 0547b64597cb21f35386e205556af466-1.jpg"
 
-set "TranscodedWallpaper=TranscodedWallpaper"
+set "TranscodedWallpaper=0547b64597cb21f35386e205556af466-1.jpg"
 set "0547b64597cb21f35386e205556af466-1.jpg"
 
 set "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
