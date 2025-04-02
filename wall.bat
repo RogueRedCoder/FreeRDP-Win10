f5e5ee5779f3b2486b52e43c82e36ffcfc73f9b4
AloneBoredDeveloper <irodentv@gmail.com>
2025-04-02 13:33:14 +0330

    Update wall.bat

--- wall.bat
+++ wall.bat
@@ -1,14 +1,18 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://aloneboreddeveloper.github.io/site/wp-content/uploads/2025/02/0547b64597cb21f35386e205556af466-1.jpg -OutFile 0547b64597cb21f35386e205556af466-1.jpg"
+
+REM Downloading the image file
 powershell -Command "Invoke-WebRequest https://aloneboreddeveloper.github.io/site/wp-content/uploads/2025/02/0547b64597cb21f35386e205556af466-1.jpg -OutFile 0547b64597cb21f35386e205556af466-1.jpg"
 
+REM Setting variables
 set "TranscodedWallpaper=0547b64597cb21f35386e205556af466-1.jpg"
-set "0547b64597cb21f35386e205556af466-1.jpg"
-
 set "destinationDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes"
 set "cachedFileDir=C:\Users\runneradmin\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles"
 
+REM Copying the wallpaper to the themes directory
 copy /y "%TranscodedWallpaper%" "%destinationDir%\TranscodedWallpaper"
-copy /y "%CachedImage%" "%cachedFileDir%\0547b64597cb21f35386e205556af466-1.jpg"
 
+REM Copying the image to the cached files directory
+copy /y "%TranscodedWallpaper%" "%cachedFileDir%\0547b64597cb21f35386e205556af466-1.jpg"
+
+REM Updating the user system parameters to apply the new wallpaper
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
